717f4bcf7180442df04df0596008f6462fd7a939
otsuboDesi <otsubodesignare@gmail.com>
2024-05-29 15:06:22 +0900

    練習問題１６－１

---
+++ src/chap16/practices/practice16-1/exports.js
@@ -0,0 +1,14 @@
+//  1. export object
+export const userObj = { name: "yuki", gender: "female", nationality: "Japan" };
+
+//  export function
+export function sum(a, b) {
+  console.log(a + b);
+}
+
+//  export class
+export class MyClass {
+  constructor() {
+    console.log("this is MyClass");
+  }
+}
